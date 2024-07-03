a2eb941372f7ba24fcef07817a2b504c3f3efcdd
DEEPTESH ROUT <153610471+deeptesh-rout@users.noreply.github.com>
2024-07-03 19:58:53 +0530

    Update Main

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,70 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import java.io.*
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.spec.SecretKeySpec
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+    val key: SecretKey = generateAESKey() // Generate AES key
+    val inputFile = File("input.txt")
+    val encryptedFile = File("encrypted.txt")
+    val decryptedFile = File("decrypted.txt")
+
+    // Encrypt file
+    encrypt(inputFile, encryptedFile, key)
+
+    // Decrypt file
+    decrypt(encryptedFile, decryptedFile, key)
+}
+
+fun encrypt(inputFile: File, encryptedFile: File, key: SecretKey) {
+    try {
+        val cipher = Cipher.getInstance("AES")
+        cipher.init(Cipher.ENCRYPT_MODE, key)
+
+        val inputStream = FileInputStream(inputFile)
+        val inputBytes = ByteArray(inputFile.length().toInt())
+        inputStream.read(inputBytes)
+
+        val outputBytes = cipher.doFinal(inputBytes)
+
+        val outputStream = FileOutputStream(encryptedFile)
+        outputStream.write(outputBytes)
+
+        inputStream.close()
+        outputStream.close()
+
+        println("File encrypted successfully.")
+    } catch (ex: Exception) {
+        ex.printStackTrace()
     }
+}
+
+fun decrypt(encryptedFile: File, decryptedFile: File, key: SecretKey) {
+    try {
+        val cipher = Cipher.getInstance("AES")
+        cipher.init(Cipher.DECRYPT_MODE, key)
+
+        val inputStream = FileInputStream(encryptedFile)
+        val inputBytes = ByteArray(encryptedFile.length().toInt())
+        inputStream.read(inputBytes)
+
+        val outputBytes = cipher.doFinal(inputBytes)
+
+        val outputStream = FileOutputStream(decryptedFile)
+        outputStream.write(outputBytes)
+
+        inputStream.close()
+        outputStream.close()
+
+        println("File decrypted successfully.")
+    } catch (ex: Exception) {
+        ex.printStackTrace()
+    }
+}
+
+fun generateAESKey(): SecretKey {
+    val keyGenerator = KeyGenerator.getInstance("AES")
+    keyGenerator.init(128) // 128-bit AES key
+    return keyGenerator.generateKey()
 }
